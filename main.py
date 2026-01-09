fe202fa4cb26c8b9a7cf28db5f85a76037165639
Jenikh <164548559+Jenikh@users.noreply.github.com>
2026-01-09 15:09:56 +0100

    Update main.py

--- main.py
+++ main.py
@@ -52,5 +52,5 @@ def add():
     return jsonify(get_items())
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 80))
     app.run(host="0.0.0.0", port=port)
